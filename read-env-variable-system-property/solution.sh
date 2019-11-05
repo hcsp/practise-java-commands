3eddc0660fecdd12d1d7dfe86b7f4da7daaca1f6
xuanzebin <xuanzebin@126.com>
2019-11-06 00:29:25 +0800

    Temp

--- read-env-variable-system-property/solution.sh
+++ read-env-variable-system-property/solution.sh
@@ -5,4 +5,8 @@
 # 3. 在当前目录中，使用java命令运行ReadEnvironmentVariableAndSystemProperty，并传递系统属性（System property）BBB，值为'（一个单引号）
 # 使得该java命令输出：
 # Environment variable AAA: $A;
-# System property BBB: '
+# System property BBB:
+
+export AAA='$A;'
+javac ReadEnvironmentVariableAndSystemProperty.java
+java -DBBB="'" ReadEnvironmentVariableAndSystemProperty
