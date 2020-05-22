87e46d81bdfce50b670e2837cf8f735c87c93020
Administrator <756433556@qq.com>
2020-05-22 14:59:42 +0800

    javac与java命令练习

--- read-env-variable-system-property/solution.sh
+++ read-env-variable-system-property/solution.sh
@@ -6,3 +6,5 @@
 # 使得该java命令输出：
 # Environment variable AAA: $A;
 # System property BBB: '
+javac ReadEnvironmentVariableAndSystemProperty.java
+java -DBBB="'" ReadEnvironmentVariableAndSystemProperty
