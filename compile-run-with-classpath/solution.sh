a0ac7c94459f25e322ac6660eb136e88e4f7d4f2
qipengWei <webqipeng@163.com>
2020-11-29 23:45:27 +0800

    修修改

--- compile-run-with-classpath/solution.sh
+++ compile-run-with-classpath/solution.sh
@@ -9,5 +9,5 @@
 # Third argument is blank: false
 
 javac -cp commons-lang3-3.9.jar StringIsBlank.java
-java -cp commons-lang3-3.9.jar;. StringIsBlank '' '' '*'
+java  -cp commons-lang3-3.9.jar:./ StringIsBlank "" ' ' '*'
 
