ab5eaece56a716794aeefb8ec1da20126f99f90b
iRuby <1479867360@qq.com>
2020-12-13 16:19:56 +0800

    我怀疑这是个bug

--- read-env-variable-system-property/ReadEnvironmentVariableAndSystemProperty.java
+++ read-env-variable-system-property/ReadEnvironmentVariableAndSystemProperty.java
@@ -1,6 +1,6 @@
 public class ReadEnvironmentVariableAndSystemProperty {
-  public static void main(String[] args) {
-    System.out.println("Environment variable AAA: " + System.getenv("AAA"));
-    System.out.println("System property BBB: " + System.getProperty("BBB"));
-  }
+    public static void main(String[] args) {
+        System.out.println("Environment variable AAA: " + System.getenv("AAA") + ";");
+        System.out.println("System property BBB: " + System.getProperty("BBB"));
+    }
 }
